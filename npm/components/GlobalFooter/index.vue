bd2c4fc46ac9566989591c58e0e72a39ae216977
jackyzm <jackyzm@foxmail.com>
2018-11-14 10:50:10 +0800

    update npm

--- npm/components/GlobalFooter/index.vue
+++ npm/components/GlobalFooter/index.vue
@@ -3,43 +3,28 @@
         <div class="links" v-if="links">
             <a v-for="link in links" :key="link.key" :target="link.blankTarget ? '_blank' : '_self'" :href="link.href">
                 <span v-if="link.title != 'github'">{{ link.title }}</span>
-                <span v-else=""><Icon :type="link.title" /></span>
+                <span v-else=""><a-icon :type="link.title" /></span>
             </a>
         </div>
-        <div class="copyright">Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品</div>
+        <div v-if="$slots.copyright" class="copyright">
+            <slot name="copyright" />
+        </div>
+        <div v-else class="copyright">Copyright <a-icon type="copyright" /> 2018 蚂蚁金服体验技术部出品</div>
     </div>
 </template>
 <script>
-import { Icon } from "ant-design-vue";
 export default {
   name: "GlobalFooter",
-  components: {
-    Icon
+  props: {
+    links: {
+      type: Array,
+      default: () => {
+        [];
+      }
+    }
   },
   data() {
-    return {
-      links: [
-        {
-          key: "Pro 首页",
-          title: "Pro 首页",
-          href: "https://github.com/Jackyzm/ant-design-vue-pro",
-          blankTarget: true
-        },
-        {
-          key: "github",
-          title: "github",
-          href: "https://github.com/Jackyzm/ant-design-vue-pro",
-          blankTarget: true
-        },
-        {
-          key: "Ant Design Vue",
-          title: "Ant Design Vue",
-          href:
-            "https://vuecomponent.github.io/ant-design-vue/docs/vue/introduce/",
-          blankTarget: true
-        }
-      ]
-    };
+    return {};
   }
 };
 </script>
